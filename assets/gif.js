8d76a3bd9e71a1d239a1b0902319a2b5ed055fa2
RealsSec <94591801+RealsSec@users.noreply.github.com>
2024-06-19 06:50:40 -0400

    1.12bv

--- assets/gif.js
+++
@@ -1,9 +0,0 @@
-const floatingGif = document.getElementById('floating-gif');
-
-floatingGif.addEventListener('mouseover', () => {
-  floatingGif.style.transform = 'scale(1.1)'; /* Increase size slightly */
-});
-
-floatingGif.addEventListener('mouseout', () => {
-  floatingGif.style.transform = 'scale(1)';  /* Reset to normal size */
-});
